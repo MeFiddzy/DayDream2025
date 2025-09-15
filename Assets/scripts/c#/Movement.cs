437fcbe7a0c557ba9cec30fe9d6ca00226d16e2b
MeFiddzy <fiddzy.pg@gmail.com>
2025-09-15 17:45:42 +0300

    Wall Jumps, Double Jumps, Double Dashing

--- Assets/scripts/c#/Movement.cs
+++ Assets/scripts/c#/Movement.cs
@@ -15,12 +15,12 @@ public class Movement : MonoBehaviour
     public LayerMask groundLayer;
     public float dashCooldown = 4.5f;
     public float dashDuration = 0.2f;
-    public string dashKey = "PlayerDash";
+    public uint wallJumps = 2;
 
     private Rigidbody2D m_rigidbody;
 
-    private Slider m_dashSlider = new Slider();
-    
+    private uint m_wallJumpsUsed = 0;
+    private bool m_airJumpedThis = false;
     private Direction m_lastDirection = Direction.LEFT;
     private float m_dashCooldownTimeLeft = 0.0f;
     private float m_dashTimeRemaining = 0.0f;
@@ -29,7 +29,6 @@ public class Movement : MonoBehaviour
     private void Awake()
     {
         m_spriteRenderer = GetComponent<SpriteRenderer>();
-        m_dashSlider = Slider.load(dashKey);
         m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -57,10 +56,52 @@ public class Movement : MonoBehaviour
         };
         
         bool onGround = raycast4OnGround(0) || raycast4OnGround(transform.localScale.x / 2) || raycast4OnGround(transform.localScale.x / -2);
-        
-        if (Input.GetKeyDown(KeyCode.UpArrow) && onGround)
+        bool canDash = m_dashCooldownTimeLeft <= 0f;
+
+        if (onGround)
         {
-            m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x, jumpPower * 2 + m_rigidbody.velocity.y);
+            m_wallJumpsUsed = 0;
+            m_airJumpedThis = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (onGround)
+                m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x, jumpPower * 2 + m_rigidbody.velocity.y);
+            else if (!m_airJumpedThis)
+            {
+                m_airJumpedThis = true;
+                m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x, jumpPower * 1.5f + m_rigidbody.velocity.y);
+
+                if (Input.GetKey(KeyCode.X))
+                {
+                    canDash = false;
+                    
+                    m_dashTimeRemaining = dashDuration;
+                    m_dashCooldownTimeLeft = dashCooldown;
+                    
+                }
+            }
+            else if (Physics2D.Raycast(
+                         new Vector2(transform.position.x + transform.localScale.x / 2, transform.position.y),
+                         Vector2.right,
+                         0.1f,
+                         groundLayer
+                     ) && m_wallJumpsUsed + 1 < wallJumps)
+            {
+                m_rigidbody.velocity = new Vector2(-(jumpPower * 3.0f), jumpPower * 1.2f);
+                m_wallJumpsUsed++;
+            }
+            else if (Physics2D.Raycast(
+                          new Vector2(transform.position.x - transform.localScale.x / 2, transform.position.y),
+                          Vector2.left,
+                          0.1f,
+                          groundLayer
+                      )&& m_wallJumpsUsed + 1 < wallJumps)
+            {
+                m_rigidbody.velocity = new Vector2((jumpPower * 3.0f), jumpPower * 1.2f);
+                m_wallJumpsUsed++;
+            }
         }
         
         if (m_dashTimeRemaining > 0f)
@@ -69,7 +110,7 @@ public class Movement : MonoBehaviour
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.X) && m_dashCooldownTimeLeft <= 0f)
+        if (Input.GetKeyDown(KeyCode.X) && canDash)
         {
             m_dashTimeRemaining = dashDuration;
             m_dashCooldownTimeLeft = dashCooldown;
